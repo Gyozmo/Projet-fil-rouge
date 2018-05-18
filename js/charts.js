ef84e610a9cadcb3bf7bca9b20f3b3e917f862c4
philippe <huhmiel@free.fr>
2018-05-18 15:42:12 +0200

    highcharts config

--- js/charts.js
+++ js/charts.js
@@ -8,7 +8,7 @@ Highcharts.chart('container', {
     xAxis: {
         categories: ['Maquetter une application', 'Concevoir une base de données', 'Mettre en place une base de données',
                      'Développer une interface utilisateur', 'Développer des composants d\'accès aux données',
-                    'Développer des pages web en lien avec une base de données','Mettre en œuvre une solution de gestion de contenu ou e-commerce','Développer une application simple de mobilité numérique','Utiliser l’anglais dans son activité professionnelle en informatique']
+                    'Développer des pages web en lien avec une base de données','Mettre en œuvre une solution de gestion de contenu ou e-commerce','Développer une application simple de mobilité numérique','Utiliser l’anglais dans son activité professionnelle en informatique', 'Actualiser et partager ses competences en informatique','Organiser son activité en developpement informatique']
     },
     yAxis: {
         min: 0,
@@ -26,7 +26,7 @@ Highcharts.chart('container', {
     },
     series: [{
         name: 'John',
-        data: [5, 3, 4, 7, 2]
+        data: [5, 3, 4, 7, 2, 6, 5, 5, 5, 5, 5]
     }, {
         name: 'Jane',
         data: [2, 2, 3, 2, 1]
