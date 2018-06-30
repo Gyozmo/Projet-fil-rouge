9673a5b3092c82fd30c3bbd63ff499a9c0b2978e
philippe <huhmiel@free.fr>
2018-06-30 23:42:17 +0200

    essentially code cleanup on sql queries

--- server.js
+++ server.js
@@ -146,62 +146,49 @@ app.get('/logout', function (req, res) {
 
 
 //////// GESTION DES COMPETENCES//////////////////////////
-////partie guillaume
+//page Modifiez vos Competences
 app.get('/comp', function (req, res) {
     res.render('comp')
 });
 
+//Reception et traitement des competences
 app.post('/comp', function (req, res) {
-    //let compValue1 = req.body.comp1; //10 variables ? -_-
-    //con.connect(function (err) {
-        //if (err) throw err;
-       // console.log('connected');
-        var sql = 'INSERT INTO note (client_idclientvoter, client_idclientvotant, comp_idcomp, note) VALUES (' + req.user.idclient + ',' + req.user.idclient + ',' +
-            1 + ',' + req.body.comp1 + '),(' + req.user.idclient + ',' + req.user.idclient + ',' +
-            2 + ',' + req.body.comp2 + '),(' + req.user.idclient + ',' + req.user.idclient + ',' +
-            3 + ',' + req.body.comp3 + '),(' + req.user.idclient + ',' + req.user.idclient + ',' +
-            4 + ',' + req.body.comp4 + '),(' + req.user.idclient + ',' + req.user.idclient + ',' +
-            5 + ',' + req.body.comp5 + '),(' + req.user.idclient + ',' + req.user.idclient + ',' +
-            6 + ',' + req.body.comp6 + '),(' + req.user.idclient + ',' + req.user.idclient + ',' +
-            7 + ',' + req.body.comp7 + '),(' + req.user.idclient + ',' + req.user.idclient + ',' +
-            8 + ',' + req.body.comp8 + '),(' + req.user.idclient + ',' + req.user.idclient + ',' +
-            9 + ',' + req.body.comp9 + '),(' + req.user.idclient + ',' + req.user.idclient + ',' +
-            10 + ',' + req.body.comp10 + ')';
-
-
-        console.log(sql)
-
-
-
-        //var sql = "INSERT INTO note (client_idclientvoter, client_idclientvotant, comp_idcomp, note) VALUES ('1','1','1','20')";
+    let sqldelete = `DELETE FROM note WHERE client_idclientvoter = ${req.user.idclient} AND client_idclientvotant = ${req.user.idclient}`;
+    con.query(sqldelete, function (err, result) {
+        console.log(result)
+            if (err) throw err;
+            console.log('ancienne notes votée supprimmées');
+    //Insere les competences perso
+        var sql = `INSERT INTO note (client_idclientvoter, client_idclientvotant, comp_idcomp, note) VALUES 
+(${req.user.idclient}, ${req.user.idclient}, 1, ${req.body.comp1}),
+(${req.user.idclient}, ${req.user.idclient}, 2, ${req.body.comp2}),
+(${req.user.idclient}, ${req.user.idclient}, 3, ${req.body.comp3}),
+(${req.user.idclient}, ${req.user.idclient}, 4, ${req.body.comp4}),
+(${req.user.idclient}, ${req.user.idclient}, 5, ${req.body.comp5}),
+(${req.user.idclient}, ${req.user.idclient}, 6, ${req.body.comp6}),
+(${req.user.idclient}, ${req.user.idclient}, 7, ${req.body.comp7}),
+(${req.user.idclient}, ${req.user.idclient}, 8, ${req.body.comp8}),
+(${req.user.idclient}, ${req.user.idclient}, 9, ${req.body.comp9}),
+(${req.user.idclient}, ${req.user.idclient}, 10, ${req.body.comp10})`;
         con.query(sql, function (err, result) {
             if (err) throw err;
-            console.log('note ajoutée')
+            console.log('note ajoutée');
+            res.redirect('/stat')
         });
     });
-
-    /*con.query("SELECT * FROM `comp`", function () {
-        //console.log(query)
-    })
-
-
-    console.log('mon log' + compValue1)
-
-    res.redirect('/comp')*/
-//});
+});
 
 //Affiche la page des votes
 app.get('/compVote', function (req, res) {
-    var sql = 'SELECT * FROM client WHERE login != "'+ req.user.login + '"'; 
+    var sql = `SELECT * FROM client WHERE login != '${req.user.login}'`; 
     console.log(sql)
     con.query(sql, function (err, userlist) {
-        console.log('USERLIST: ', userlist);
+        //console.log('USERLIST: ', userlist);
         
         res.render('compVote',{
             users: userlist
         })
-    });
-    
+    });   
 });
 
 //Recupere les votes et les insert dans la db
@@ -210,30 +197,31 @@ app.post('/compVote', function (req, res){
     con.query(sqldelete, function (err, result) {
             if (err) throw err;
             console.log('ancienne notes votée supprimmées');
-        let sql = 'INSERT INTO note (client_idclientvoter, client_idclientvotant, comp_idcomp, note) VALUES (' + req.body.idVoted + ',' + req.user.idclient + ',' +
-            1 + ',' + req.body.comp1 + '),(' + req.body.idVoted + ',' + req.user.idclient + ',' +
-            2 + ',' + req.body.comp2 + '),(' + req.body.idVoted + ',' + req.user.idclient + ',' +
-            3 + ',' + req.body.comp3 + '),(' + req.body.idVoted + ',' + req.user.idclient + ',' +
-            4 + ',' + req.body.comp4 + '),(' + req.body.idVoted + ',' + req.user.idclient + ',' +
-            5 + ',' + req.body.comp5 + '),(' + req.body.idVoted + ',' + req.user.idclient + ',' +
-            6 + ',' + req.body.comp6 + '),(' + req.body.idVoted + ',' + req.user.idclient + ',' +
-            7 + ',' + req.body.comp7 + '),(' + req.body.idVoted + ',' + req.user.idclient + ',' +
-            8 + ',' + req.body.comp8 + '),(' + req.body.idVoted + ',' + req.user.idclient + ',' +
-            9 + ',' + req.body.comp9 + '),(' + req.body.idVoted + ',' + req.user.idclient + ',' +
-            10 + ',' + req.body.comp10 + ')';
+        let sql = `INSERT INTO note (client_idclientvoter, client_idclientvotant, comp_idcomp, note) VALUES 
+(${req.body.idVoted}, ${req.user.idclient}, 1, ${req.body.comp1}),
+(${req.body.idVoted}, ${req.user.idclient}, 2, ${req.body.comp2}),
+(${req.body.idVoted}, ${req.user.idclient}, 3, ${req.body.comp3}),
+(${req.body.idVoted}, ${req.user.idclient}, 4, ${req.body.comp4}),
+(${req.body.idVoted}, ${req.user.idclient}, 5, ${req.body.comp5}),
+(${req.body.idVoted}, ${req.user.idclient}, 6, ${req.body.comp6}),
+(${req.body.idVoted}, ${req.user.idclient}, 7, ${req.body.comp7}),
+(${req.body.idVoted}, ${req.user.idclient}, 8, ${req.body.comp8}),
+(${req.body.idVoted}, ${req.user.idclient}, 9, ${req.body.comp9}),
+(${req.body.idVoted}, ${req.user.idclient}, 10, ${req.body.comp10})`;
         con.query(sql, function (err, result) {
             if (err) throw err;
             console.log('note votée ajoutée');
+            res.redirect('/stat');
         });
     });
 });
 
-//Recupere les notes perso sur la db, et les affiche
+//Recupere les notes perso et les votes sur la db, et les affiche
 app.get('/stat', function (req, res) {
     console.log("page stat request");
     let idclient = req.user.idclient;
     //requete vers notes perso
-    let sql = "SELECT * FROM note WHERE client_idclientvoter = " + idclient + " AND client_idclientvotant = " + idclient;
+    let sql = `SELECT * FROM note WHERE client_idclientvoter = ${idclient} AND client_idclientvotant = ${idclient}`;
     //requete vers notes votée
     let sqlVote = `
 SELECT AVG(note) AS avg FROM note WHERE ( client_idclientvoter = ${idclient} AND client_idclientvotant != ${idclient} AND comp_idcomp = 1)
@@ -255,11 +243,13 @@ UNION ALL
 SELECT AVG(note) AS avg FROM note WHERE ( client_idclientvoter = ${idclient} AND client_idclientvotant != ${idclient} AND comp_idcomp = 9)
 UNION ALL
 SELECT AVG(note) AS avg FROM note WHERE ( client_idclientvoter = ${idclient} AND client_idclientvotant != ${idclient} AND comp_idcomp = 10)`;
-    //console.log(sqlVote)
+    //Lancement des requetes
+    let compArr;
     con.query(sql, function (err, result) {
         if (err) throw err;
-        
-        let compArr = [Number(result[0].note),
+        console.log(result)
+        if (result[0]){
+            compArr = [Number(result[0].note),
                        Number(result[1].note),
                        Number(result[2].note),
                        Number(result[3].note),
@@ -269,10 +259,9 @@ SELECT AVG(note) AS avg FROM note WHERE ( client_idclientvoter = ${idclient} AND
                        Number(result[7].note),
                        Number(result[8].note),
                        Number(result[9].note)];
+        }
         con.query(sqlVote, function (err, result2) {
-            if (err) throw err;
-            let test = "AVG (note)"
-            console.log("AVG: ",result2[0]);
+            //if (err) res.redirect('/comp');//throw err;
             let voteArr =[Number(result2[0].avg),
                        Number(result2[1].avg),
                        Number(result2[2].avg),
@@ -283,20 +272,20 @@ SELECT AVG(note) AS avg FROM note WHERE ( client_idclientvoter = ${idclient} AND
                        Number(result2[7].avg),
                        Number(result2[8].avg),
                        Number(result2[9].avg)]
-            res.render('stat', {
-                title: "Statistiques",
-                user: req.user.login,
-                dataUser: compArr,
-                dataVote: voteArr
-            });
+            console.log(result)
+            if (result.length ){
+                res.render('stat', {
+                    title: "Statistiques",
+                    user: req.user.login,
+                    dataUser: compArr,
+                    dataVote: voteArr
+                });
+            } else {
+                res.redirect('/comp')
+            }
         });
     });
 });
-
-
-//Recupere  
-
-
 
 /////////// FUNCTION //////////////
 //check if user is logged in
@@ -308,7 +297,6 @@ function loggedIn(req, res, next) {
     }
 }
 
-
 //////////// SQL ///////////////////////
 //connection parameters
 var con = mysql.createConnection({
@@ -318,23 +306,9 @@ var con = mysql.createConnection({
     database: "sql7244511"
 });
 
-
 //Genere un log lors des erreurs sql
 con.on('error', function (err) {
     console.log("[mysql error]", err);
 });
-
-function recupNote(idclient) {
-
-    //con.connect(function (err) {
-    //if (err) throw err;
-    //console.log('connected');
-    var sql = "SELECT * FROM note WHERE client_idclientvoter = " + idclient + " AND client_idclientvotant = " + idclient;
-    con.query(sql, function (err, result) {
-        if (err) throw err;
-        console.log("RESULT NOTES", result)
-    })
-    //})
-}
 
 ///////////// SQL END ////////////////////
